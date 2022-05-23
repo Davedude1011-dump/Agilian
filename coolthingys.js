72a2fd89bbf248a704f86ffa9fbd2efeee5d7cde
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-05-23 19:39:31 +0100

    Add files via upload

--- coolthingys.js
+++ coolthingys.js
@@ -1,7 +1,7 @@
 function logIn() {
     if (document.getElementById("username").value === "Agilian") {
         if (document.getElementById("password").value === "54465") {
-            
+            document.getElementById("logInText").innerHTML = "Correct"
         }
         else {
             document.getElementById("logInText").innerHTML = "Wrong Password"
